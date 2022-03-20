7fa3128d0da17ac50dc57faf99d1f8ead132de2e
cosinechicken <ah.blenden@gmail.com>
2022-03-20 04:23:07 -0400

    Implemented Randomization
    The button now randomly selects one of the songs. Most of the difficult work is now complete.

--- data.js
+++ data.js
@@ -1,6 +1,11 @@
 // A data file containing a dictionary sending IDs to song titles. 
 // IDs are 4 digits, where first digit corresponds to type (e.g. classical, anime, etc.)
 var dict = {
-  "0000": "Chopin Nocturne Op. 15 No. 2",
-  "0001": "Chopin Nocturne Op. 27 No. 2"
+  "0000": "Chopin Nocturne Op. 15 No. 2", // https://www.youtube.com/watch?v=cnnSeChY--M
+  "0001": "Chopin Nocturne Op. 27 No. 2", // https://www.youtube.com/watch?v=WJ8RVjm49hE
 };
+
+var idPair = []
+for (const [key, value] of Object.entries(dict)) {
+  idPair.push([key, value]);
+}
---
+++ player.js
@@ -0,0 +1,9 @@
+function randomize() {
+    // Randomizes song being played
+    let id = Math.floor(Math.random() * idPair.length);
+    // Find the string of the title
+    let srcString = "Music/" + idPair[id][1] + ".mp4";
+    console.log(srcString);
+    document.getElementById("audio").src = srcString;
+    document.getElementById("current").textContent = "Currently Playing: " + idPair[id][1];
+}
