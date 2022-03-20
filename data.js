6aad49ea596f91650fe5556716b3b98b28a2210b
cosinechicken <ah.blenden@gmail.com>
2022-03-20 03:40:36 -0400

    Moved to github
    Moved changes from S3 site to github.

---
+++ data.js
@@ -0,0 +1,6 @@
+// A data file containing a dictionary sending IDs to song titles. 
+// IDs are 4 digits, where first digit corresponds to type (e.g. classical, anime, etc.)
+var dict = {
+  "0000": "Chopin Nocturne Op. 15 No. 2",
+  "0001": "Chopin Nocturne Op. 27 No. 2"
+};
