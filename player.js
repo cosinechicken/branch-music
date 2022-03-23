68f73062e8bd8cd71bd2f140001ea2c337245b3e
cosinechicken <ah.blenden@gmail.com>
2022-03-23 08:42:26 -0400

    Successfully implemented play by ID
    Next step is to improve the CSS

--- player.js
+++ player.js
@@ -28,3 +28,23 @@ function audioEnded() {
         document.getElementById("audio").play();
     }
 }
+function playByID() {
+    let id = document.getElementById("idInput").value;
+    document.getElementById("idInput").value = "";
+    console.log("Now playing: " + id);
+    // Run this function to play a specific song based on id
+    for (let i = 0; i < idPair.length; i++) {
+        if (id === idPair[i][0]) {
+            // We found a match
+            let srcString = "Music/" + idPair[i][1] + ".mp4";
+            console.log(srcString);
+            document.getElementById("audio").src = srcString;
+            document.getElementById("current").textContent = "Currently Playing: " + idPair[i][1] 
+        + " (ID: " + idPair[i][0] + ")";
+            document.getElementById("audio").play();
+            return;
+        }
+    }
+    document.getElementById("current").textContent = "Currently Playing: undefined"
+    document.getElementById("audio").src = "";
+}
