2556098518ddd625f1bcb94a5d10f9b7d695262a
cosinechicken <ah.blenden@gmail.com>
2022-03-31 14:42:22 -0400

    Made songlist table sortable
    Paving the way to add time and volume columns in the table.

--- songList.js
+++ songList.js
@@ -1,10 +1,11 @@
 
-var table = document.getElementById("songTable");
+var table = document.getElementById("songTable").getElementsByTagName('tbody')[0];
 console.log(table);
 generateTable(table, idPair);
 
 function generateTable(table, data) {
     for (let element of data) {
+        console.log(element);
         let row = table.insertRow(-1);
         for (key in element) {
             let cell = row.insertCell();
