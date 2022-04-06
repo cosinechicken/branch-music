1aef77e5cc135f26d5e50ae8721d6bbc52c2d200
cosinechicken <ah.blenden@gmail.com>
2022-04-06 16:19:06 -0400

    Updated UI
    Added Volume text field and decreased size of text.

--- player.js
+++ player.js
@@ -3,14 +3,8 @@ var loop = false;
 function randomize() {
     // Randomizes song being played
     console.log(catInfoList.length)
-    let id = Math.floor(Math.random() * catInfoList.length);
-    // Find the string of the title
-    let srcString = "Music/" + catInfoList[id][1] + ".mp4";
-    console.log(srcString);
-    document.getElementById("audio").src = srcString;
-    document.getElementById("current").textContent = "Currently Playing: " + catInfoList[id][1] 
-        + " (ID: " + catInfoList[id][0] + ")";
-    document.getElementById("audio").play();
+    let index = Math.floor(Math.random() * catInfoList.length);
+    play(index);
 }
 function toggleAutoplay() {
     // Toggle autoplay button
@@ -31,21 +25,25 @@ function playByID() {
     // Run this function to play a specific song based on id
     for (let i = 0; i < infoList.length; i++) {
         if (id === infoList[i][0]) {
-            // We found a match
-            let srcString = "Music/" + infoList[i][1] + ".mp4";
-            console.log(srcString);
-            document.getElementById("audio").src = srcString;
-            document.getElementById("current").textContent = "Currently Playing: " + infoList[i][1] 
-        + " (ID: " + infoList[i][0] + ")";
-            document.getElementById("audio").play();
-            // Update the category
-            document.getElementById("categorySelect").value = categories[infoList[i][1]];
-            changeCategory();
+            play(i);
             return;
         }
     }
-    document.getElementById("current").textContent = "Currently Playing: undefined"
+    document.getElementById("current").textContent = "Playing: undefined"
     document.getElementById("audio").src = "";
+}
+function play(index) {
+    // We found a match
+    let srcString = "Music/" + infoList[index][1] + ".mp4";
+    console.log(srcString);
+    document.getElementById("audio").src = srcString;
+    document.getElementById("current").textContent = "Playing: " + infoList[index][1]
+        + " (ID: " + infoList[index][0] + ")";
+    document.getElementById("volume").textContent = "Volume: " + infoList[index][3];
+    document.getElementById("audio").play();
+    // Update the category
+    document.getElementById("categorySelect").value = categories[infoList[index][1]];
+    changeCategory();
 }
 function changeCategory() {
     // Called when category select is changed, updates the list of songs which can be played
